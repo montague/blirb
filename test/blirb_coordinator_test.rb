689b4d220a33504f83d823ab8787fdee0c686697
ian asaff <iasaff@where.com>
2011-07-16 12:51:28 -0400

    takes an optional tasks file from the command line in yml format

--- test/blirb_coordinator_test.rb
+++ test/blirb_coordinator_test.rb
@@ -1,16 +1,16 @@
 require 'test/unit'
 require_relative 'test_helper'
 class BlirbCoordinatorTest < Test::Unit::TestCase
-  def setup
-    @tasks = []
-    @tasks << Task.new("set an instance variable 'dude' with a value... any value.", %{ 
-      defined?(@dude) ? true : false
-    })
-    @bc = BlirbCoordinator.new @tasks
-  end
   
- def test_blirb_coordinator_setup
-  assert_equal 1, @bc.tasks.length, "bc should have tasks"
+ def test_blirb_coordinator_basics
+  bc = BlirbCoordinator.new
+  assert_equal 2, bc.tasks.length, "bc should have tasks"
   assert Object.class_eval { @blirb }, "bc should set Object class variable"
+  
+  bc = BlirbCoordinator.new 'test/fixtures/tasks.yml'
+  assert_equal 1, bc.tasks.length
  end
+  
+  
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,1 +1,2 @@
+Dir.chdir File.expand_path('../..', __FILE__) #for textmate's cmd+R
 require_relative '../lib/blirb'
